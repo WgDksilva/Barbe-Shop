b32145eaeb8eafaf89233eb870b81eeefbf7a1d8
WgDksilva <wagnersilva.dk021@gmail.com>
2025-01-10 19:35:16 -0300

    testando

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import  CreateGlobalStyles from  "./styles/globalStyles"
 
 import Router from "./routes/routes.jsx";
 
@@ -9,5 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Router />
     </BrowserRouter>
+    <CreateGlobalStyles />
   </React.StrictMode>
 )
+
